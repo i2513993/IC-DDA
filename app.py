dd640d03363e3dd6d4f3eff1c0fdff083fed9c15
Saul Yepez <i2513993@continental.edu.pe>
2026-03-27 16:16:45 -0500

    Creando la parte visual

--- app.py
+++ app.py
@@ -30,5 +30,18 @@ class Client:
 #--------------------------
 
 st.title("DEMO POO - CIENCIA DE DATOS")
-
 st.write("Ingrese los datos del cliente")
+
+nombre = st.text_input("Nombre")
+edad = st.number_input("Edad", min_value=0)
+saldo = st.number_imput("Saldo", min_value=0.0)
+
+if st.button("Crear Cliente"):
+    
+    #Secuencia
+    cliente = cliente(nombre, edad, saldo)
+
+    st.success("Clientre creado correctamente")
+
+    st.write("### Informacion de cliente")
+    st.write(cliente.mostrar_info()))
