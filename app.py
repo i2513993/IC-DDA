49998519bd27675ab165f93f037d9fa28f698548
Saul Yepez <i2513993@continental.edu.pe>
2026-03-27 15:55:07 -0500

    Creando las clases con las funciones

--- app.py
+++ app.py
@@ -1,1 +1,34 @@
-print("hola AleLucca")
+import streamlit as st
+
+#--------------------------
+#CLASE POO
+#--------------------------
+
+class Client:
+    def __init__(self, nombre, edad, saldo):
+        self.nombre = nombre
+        self.edad = edad
+        self.saldo = saldo
+    
+    #GETTERS (EMCAPSULAMIENTO)
+    def get_nombre(self)
+        return self.__nombre
+
+    def get_edad(self)
+        return self.__edad
+
+    def get_saldo(self)
+        return self.__saldo
+
+
+    # METODO
+    def mostrar_info(self):
+        return f"Cliente: {self.nombre}, Edad: {self.edad}, Saldo: {self.saldo}"
+
+#--------------------------
+#STREAMLIT
+#--------------------------
+
+st.title("DEMO POO - CIENCIA DE DATOS")
+
+st.write("Ingrese los datos del cliente")
