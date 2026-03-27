f0dafa55a891acf5d5db45583d24ca3c17c25f9c
Saul Yepez <i2513993@continental.edu.pe>
2026-03-27 17:21:21 -0500

    modularizando app 3

--- services/cliente_services.py
+++ services/cliente_services.py
@@ -2,6 +2,5 @@ from models.cliente import Cliente
 
 def crear_cliente(nombre, edad, saldo):
     if edad < 18:
-        raise ValueError ("El cliente debe ser mayor de edad")
-
+        raise ValueError("El cliente debe ser mayor de edad")
     return Cliente(nombre, edad, saldo)
