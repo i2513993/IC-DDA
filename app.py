693c2f0c7b58c47d709665e19a92efa766428195
Saul Yepez <i2513993@continental.edu.pe>
2026-03-27 16:58:27 -0500

    modularizando app

--- app.py
+++ app.py
@@ -1,33 +1,7 @@
 import streamlit as st
+from models.cliente import Cliente
+from services.cliente_services import crear_cliente
 
-#--------------------------
-#CLASE POO
-#--------------------------
-
-class Cliente:
-    def __init__(self, nombre, edad, saldo):
-        self.nombre = nombre
-        self.edad = edad
-        self.saldo = saldo
-    
-    #GETTERS (EMCAPSULAMIENTO)
-    def get_nombre(self):
-        return self.__nombre
-
-    def get_edad(self):
-        return self.__edad
-
-    def get_saldo(self):
-        return self.__saldo
-
-
-    # METODO
-    def mostrar_info(self):
-        return f"Cliente: {self.nombre}, Edad: {self.edad}, Saldo: {self.saldo}"
-
-#--------------------------
-#STREAMLIT
-#--------------------------
 
 st.title("DEMO POO - CIENCIA DE DATOS")
 st.write("Ingrese los datos del cliente")
@@ -37,11 +11,15 @@ edad = st.number_input("Edad", min_value= 0)
 saldo = st.number_input("Saldo", min_value= 0.0)
 
 if st.button("Crear Cliente"):
+
+    try:
+                #Secuencia
+        cliente = Cliente(nombre, edad, saldo)
+
+        st.success("Clientre creado correctamente")
+
+        st.write("### Informacion de cliente")
+        st.write(cliente.mostrar_info())
     
-    #Secuencia
-    cliente = Cliente(nombre, edad, saldo)
-
-    st.success("Clientre creado correctamente")
-
-    st.write("### Informacion de cliente")
-    st.write(cliente.mostrar_info())
+    except Exception as e:
+        st.error(str(e))
