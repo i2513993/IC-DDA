7d21eff893dfe9765f1ee76f0fe6849ab54fc3d2
Saul Yepez <i2513993@continental.edu.pe>
2026-03-27 17:04:38 -0500

    modularizando app 2

---
+++ models/cliente.py
@@ -0,0 +1,25 @@
+import streamlit as st
+#--------------------------
+#CLASE POO
+#--------------------------
+
+class Cliente:
+    def __init__(self, nombre, edad, saldo):
+        self.nombre = nombre
+        self.edad = edad
+        self.saldo = saldo
+    
+    #GETTERS (EMCAPSULAMIENTO)
+    def get_nombre(self):
+        return self.__nombre
+
+    def get_edad(self):
+        return self.__edad
+
+    def get_saldo(self):
+        return self.__saldo
+
+
+    # METODO
+    def mostrar_info(self):
+        return f"Cliente: {self.nombre}, Edad: {self.edad}, Saldo: {self.saldo}"
