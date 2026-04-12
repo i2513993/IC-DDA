3fff097096d3c8d0a52c2d5003f50f1bd0982474
Saul Yepez <i2513993@continental.edu.pe>
2026-04-12 12:42:43 -0500

    Bug en saldo negativo

--- app.py
+++ app.py
@@ -1,26 +1,19 @@
 import streamlit as st
-import pandas as pd
 from models.cliente import Cliente
 from services.cliente_services import crear_cliente
+from utils.cliente_utils import (
+    mostrar_tabla,
+    mostrar_clientes,
+    mostrar_analisis
 
+)
 
 st.title("Demo POO - Ciencia de Datos")
 st.write("Ingrese los datos del cliente:")
 
-# Inicializar lista de clientes
 if 'clientes' not in st.session_state:
     st.session_state.clientes = []
 
-def mostrar_tabla(clientes):
-    df = pd.DataFrame(clientes)
-    st.dataframe(df)
-
-def calcular_promedio (clientes):
-    suma = 0
-    for c in clientes:
-        suma += c['Saldo']
-    promedio = suma / len(clientes)
-    return promedio
 
 
 with st.form("form_cliente"):
@@ -35,6 +28,9 @@ with st.form("form_cliente"):
         try:
             if nombre == "":
                 st.warning("Debe ingresar un nombre")   
+            elif saldo < 0:
+                st.warning("El saldo no puede ser negativo")
+
             else:
                 # Crea Cliente + Mensaje
                 cliente, mensaje = crear_cliente(nombre, edad, saldo)
@@ -52,38 +48,15 @@ with st.form("form_cliente"):
 
         except ValueError as e:
             st.warning(str(e))
-        except Exception as e:
-            st.error(str(e))
+        except Exception:
+            st.error("Error inesperado")
 
 if len(st.session_state.clientes) > 0:
 
     st.write("### Tabla de Clientes")
-    df = pd.DataFrame(st.session_state.clientes) 
-    st.dataframe(df)                              
-
-    #FOR  --  Recorrer clientes
-
-    st.write("### Recorrer Clientes")
-    for c in st.session_state.clientes:
-        st.write(f"{c['Nombre']} - {c['Edad']} - {c['Saldo']}")
-
-    #WHILE --  Recorrido alternativo
-
-    st.write("### Recorrido con While")
-    i = 0
-    while i < len(st.session_state.clientes):
-        c = st.session_state.clientes[i]
-        st.write(f"{c['Nombre']} - {c['Edad']} - {c['Saldo']}")
-        i += 1
-
-    # Analisis total de clientes
-
-    contador = len(st.session_state.clientes)
-    st.write(f"Total de clientes: {contador}")
-
-    #Promedio de saldo
-    promedio = calcular_promedio(st.session_state.clientes)
-    st.write(f"Promedio de saldo: S/ {round(promedio, 2)}")
+    mostrar_tabla(st.session_state.clientes)
+    mostrar_clientes(st.session_state.clientes)
+    mostrar_analisis(st.session_state.clientes)
 
 else:
     st.write("No hay clientes registrados")
