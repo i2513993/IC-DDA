2a22621c27f59595570a773e0961ec1863d696c0
Saul Yepez <i2513993@continental.edu.pe>
2026-04-12 11:42:54 -0500

    inicializando lista de memoria

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from services.cliente_services import crear_cliente
 st.title("Demo POO - Ciencia de Datos")
 st.write("Ingrese los datos del cliente:")
 
+# Inicializar lista de clientes
+if 'clientes' not in st.session_state:
+    st.session_state.clientes = []
+
 # Inputs
 nombre = st.text_input("Nombre")
 edad = st.number_input("Edad", min_value = 0)
