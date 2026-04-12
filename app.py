230dbf589fba67564091be648390cf6b62849a49
Saul Yepez <i2513993@continental.edu.pe>
2026-04-12 11:39:57 -0500

    agregando mas reglas en app.py

--- app.py
+++ app.py
@@ -20,23 +20,52 @@ if st.button("Crear Cliente"):
         cliente, mensaje = crear_cliente(nombre, edad, saldo)
 
         st.success("Cliente creado correctamente")
-
         st.info(mensaje)
 
-        st.write("### Información del cliente: ")
-        st.write(cliente.mostrar_info())
+        #Guardar en Memoria
+        st.session_state.clientes.append({
+            "Nombre": cliente.get_nombre(),
+            "Edad": cliente.get_edad(),
+            "Saldo": cliente.get_saldo()
+        })
 
-        data = {
-            "Nombre": [cliente.get_nombre()],
-            "Edad": [cliente.get_edad()],
-            "Saldo": [cliente.get_saldo()]
-        }
-
-        df = pd.DataFrame(data)
-
-        st.write("### Tabla del Cliente")
-        st.dataframe(df)
-
-    
     except Exception as e:
         st.error(str(e))
+
+if len(st.session_state.clientes) > 0:
+
+    st.write("### Tabla de Clientes")
+    st.dataframe(pd.DataFrame(st.session_state.clientes))
+    st.dataframe (df)
+
+    #FOR  --  Recorrer clientes
+
+    st.write("### Recorrer Clientes")
+    for c in st.session_state.clientes:
+        st.write(f"{c['Nombre']} - {c['Edad']} - {c['Saldo']}")
+
+    #WHILE --  Recorrido alternativo
+
+    st.write("### Recorrido con While")
+    i = 0
+    while i < len(st.session_state.clientes):
+        c = st.session_state.clientes[i]
+        st.write(f"{c['Nombre']} - {c['Edad']} - {c['Saldo']}")
+        i += 1
+
+    # Analisis total de clientes
+
+    contador = 0
+    for c in st.session_state.clientes:
+        contador += 1
+    st.write(f"Total de clientes: {contador}")
+
+    #Promedio de saldo
+    suma = 0
+    for c in st.session_state.clientes:
+        suma += c['Saldo']
+    promedio = suma / contador
+    st.write(f"Promedio de saldo: S/ {round(promedio, 2)}")
+
+else:
+    st.write("No hay clientes registrados")
